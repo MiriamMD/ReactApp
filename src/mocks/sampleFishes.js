7161ea909c46e45fe030126bccbdb9fa5f7e8a82
Miriam Merchante <mimerchante27@gmail.com>
2018-01-28 20:54:11 +0100

    chore(mocks): include mocks

---
+++ src/mocks/sampleFishes.js
@@ -0,0 +1,18 @@
+const sampleFishes = {
+  fish1: {
+    name: 'Fish1',
+    price: 10001,
+    status: 'Available',
+    description: 'Fish 1 available',
+    image: 'Fish 1 image'
+  },
+  fish2: {
+    name: 'Fish2',
+    price: 201,
+    status: 'Sold out',
+    description: 'Fish 2 sould out',
+    image: 'Fish 2 image'
+  }
+}
+
+export default sampleFishes;
